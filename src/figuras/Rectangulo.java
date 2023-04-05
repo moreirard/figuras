54e365a1354f6d450f26235c44e8b8cee5129a19
Moreira Rubén Darío <moreirard@gmail.com>
2023-04-04 21:56:19 -0300

    Se agrega método para calcular área de rectángulo
    Se agrega método para calcular área de rectángulo

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -17,4 +17,8 @@ public class Rectangulo {
 	public String toString() {
 		return "Rectangulo [ancho=" + ancho + ", alto=" + alto + "]";
 	}	
+	
+	public Double getArea() {
+		return this.ancho * this.alto;
+	}
 }
